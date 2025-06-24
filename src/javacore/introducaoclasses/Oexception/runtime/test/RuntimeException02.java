ca575cac01a78b1adc0d6d2f90bf55855b369f44
RafaelMelo23 <eooraff@gmail.com>
2025-06-24 15:29:17 -0300

    aula98

---
+++ src/javacore/introducaoclasses/Oexception/runtime/test/RuntimeException02.java
@@ -0,0 +1,27 @@
+package javacore.introducaoclasses.Oexception.runtime.test;
+
+public class RuntimeException02 {
+
+    public static void main(String[] args) {
+
+        divisao(1, 0);
+
+        System.out.println("Codigo finalizado");
+    }
+
+
+    /**
+     *
+     * @param a
+     * @param b -- nao pode ser zero
+     * @return
+     */
+    private static int divisao(int a, int b) {
+
+        if (b == 0) {
+            throw new IllegalArgumentException("Argumento ilegal. b nao pode ser 0");
+        }
+
+        return a / b;
+    }
+}
