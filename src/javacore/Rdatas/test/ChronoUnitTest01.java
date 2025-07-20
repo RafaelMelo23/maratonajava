bdfa28c7066863e4df87fd3b6f181119d22009ae
RafaelMelo23 <eooraff@gmail.com>
2025-07-20 14:02:31 -0300

    aula125

---
+++ src/javacore/Rdatas/test/ChronoUnitTest01.java
@@ -0,0 +1,21 @@
+package javacore.Rdatas.test;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+public class ChronoUnitTest01 {
+
+    public static void main(String[] args) {
+
+        LocalDateTime aniversario = LocalDateTime.of(2005, 05, 29, 10, 15);
+
+        LocalDateTime now = LocalDateTime.now();
+
+        System.out.println(ChronoUnit.MONTHS.between(aniversario, now));
+        System.out.println(ChronoUnit.DAYS.between(aniversario, now));
+        System.out.println(ChronoUnit.HOURS.between(aniversario, now));
+        System.out.println(ChronoUnit.MINUTES.between(aniversario, now));
+        System.out.println(ChronoUnit.SECONDS.between(aniversario, now));
+        System.out.println(ChronoUnit.YEARS.between(aniversario, now));
+    }
+}
