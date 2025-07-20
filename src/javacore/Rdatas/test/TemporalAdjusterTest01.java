a948a20aee237bb0a71f6abe2c0b64871e525b1c
RafaelMelo23 <eooraff@gmail.com>
2025-07-20 16:13:42 -0300

    aula127

---
+++ src/javacore/Rdatas/test/TemporalAdjusterTest01.java
@@ -0,0 +1,42 @@
+package javacore.Rdatas.test;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.Temporal;
+import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalAmount;
+
+class ObterProximoDiaUtil implements TemporalAdjuster {
+
+    @Override
+    public Temporal adjustInto(Temporal temporal) {
+
+        DayOfWeek currentDay = DayOfWeek.from(temporal);
+        int addDay;
+
+        switch (currentDay) {
+
+            case THURSDAY -> addDay = 4;
+            case FRIDAY -> addDay = 3;
+            case SATURDAY -> addDay = 2;
+
+            default -> addDay = 1;
+        }
+
+        return temporal.plus(addDay, ChronoUnit.DAYS);
+    }
+}
+
+public class TemporalAdjusterTest01 {
+
+    public static void main(String[] args) {
+
+        LocalDate now = LocalDate.now();
+        System.out.println(now.getDayOfWeek());
+
+        now = LocalDate.now().with(new ObterProximoDiaUtil());
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+    }
+}
