f9d0d6ab272ff1649ab76ecb1a92ad2eb6caf3a3
RafaelMelo23 <eooraff@gmail.com>
2025-07-21 16:27:12 -0300

    aula130

---
+++ src/javacore/Tresourcebundle/test/ResourceBundleTest01.java
@@ -0,0 +1,16 @@
+package javacore.Tresourcebundle.test;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class ResourceBundleTest01 {
+
+    public static void main(String[] args) {
+
+        ResourceBundle bundle = ResourceBundle.getBundle("messages", Locale.of("en", "US"));
+        System.out.println(bundle.getString("good.morning"));
+
+        ResourceBundle bundleBR = ResourceBundle.getBundle("messages", Locale.of("pt", "BR"));
+        System.out.println(bundleBR.getString("good.morning"));
+    }
+}
