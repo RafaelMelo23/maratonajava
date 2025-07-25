1e12b7a80604f2f87f58fffe842a177eed81492c
RafaelMelo23 <eooraff@gmail.com>
2025-07-25 15:09:56 -0300

    aula139

---
+++ src/javacore/Vio/test/FileWriterTest01.java
@@ -0,0 +1,27 @@
+package javacore.Vio.test;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+// File
+// FileWriter
+// FileReader
+// BufferedWriter
+// BufferedReader
+public class FileWriterTest01 {
+
+    public static void main(String[] args) {
+
+        File file = new File("file.txt");
+        boolean append = true;
+
+        try (FileWriter fw = new FileWriter(file, append)) { // <- pode ser passado um boolean como param pra dar append no file, e adicionar o conteudo inves de apagar e criar um novo
+
+            fw.write("Its over anakin, i have the highground");
+            fw.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
