5d16ff2412129e7eb84a3a80dbaf322bf9acd065
RafaelMelo23 <eooraff@gmail.com>
2025-07-28 15:46:34 -0300

    aula146

---
+++ src/javacore/Vio/test/NormalizeTest01.java
@@ -0,0 +1,22 @@
+package javacore.Vio.test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class NormalizeTest01 {
+
+    public static void main(String[] args) {
+
+        String diretorioProjeto = "home/teste/test";
+        String arquivoTxt = "/../../arquivo.txt";
+
+        Path path1 = Paths.get(diretorioProjeto, arquivoTxt);
+        System.out.println(path1);
+        System.out.println(path1.normalize());
+
+        Path path2 = Paths.get("/home/./teste/./test/");
+        System.out.println(path2);
+        System.out.println(path2.normalize());
+
+    }
+}
