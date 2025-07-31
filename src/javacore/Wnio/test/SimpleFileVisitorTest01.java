38e9debfb1937e26b0bafa21a34da199c5854718
RafaelMelo23 <eooraff@gmail.com>
2025-07-31 20:38:29 -0300

    aula155

--- src/javacore/Wnio/test/SimpleFileVisitorTest01.java
+++ src/javacore/Wnio/test/SimpleFileVisitorTest01.java
@@ -15,6 +15,26 @@ class ListAllFiles extends SimpleFileVisitor<Path> {
 
         return FileVisitResult.CONTINUE;
     }
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+
+        System.out.println("pre visit"+dir.getFileName());
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+        return super.visitFileFailed(file, exc);
+    }
+
+    @Override
+    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+        System.out.println("post visit"+dir.getFileName());
+        return FileVisitResult.CONTINUE;
+    }
+
+
 }
 public class SimpleFileVisitorTest01 {
 
