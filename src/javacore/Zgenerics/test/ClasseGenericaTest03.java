0c085f6e48bf49ce9a641fd0da041504384e1baa
RafaelMelo23 <eooraff@gmail.com>
2025-08-13 12:55:23 -0300

    aula187

---
+++ src/javacore/Zgenerics/test/ClasseGenericaTest03.java
@@ -0,0 +1,29 @@
+package javacore.Zgenerics.test;
+
+import javacore.Zgenerics.dominio.Barco;
+import javacore.Zgenerics.dominio.Carro;
+import javacore.Zgenerics.service.RentalService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClasseGenericaTest03 {
+
+    public static void main(String[] args) {
+
+        List<Carro> carrosDisponiveis = new ArrayList<>(List.of(new Carro("BMW M3 E92"), new Carro("Audi RS6 Avant")));
+        List<Barco> barcosDisponiveis = new ArrayList<>(List.of(new Barco("Lancha"), new Barco("Iate")));
+
+        RentalService<Carro> rentalService = new RentalService<>(carrosDisponiveis);
+        Carro carro = rentalService.retrieveAvailableObject();
+        System.out.println("Usando o carro por um mes...");
+        rentalService.returnRentedObject(carro);
+
+        System.out.println("--------");
+
+        RentalService<Barco> rentalServiceBoat = new RentalService<>(barcosDisponiveis);
+        Barco barco = rentalServiceBoat.retrieveAvailableObject();
+        System.out.println("Usando o barco por um mes...");
+        rentalServiceBoat.returnRentedObject(barco);
+    }
+}
