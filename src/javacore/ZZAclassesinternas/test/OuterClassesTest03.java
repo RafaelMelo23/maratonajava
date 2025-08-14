0e580fb1bf69e12ba2990d90a231be67f37f775e
RafaelMelo23 <eooraff@gmail.com>
2025-08-14 12:16:46 -0300

    aula192

---
+++ src/javacore/ZZAclassesinternas/test/OuterClassesTest03.java
@@ -0,0 +1,22 @@
+package javacore.ZZAclassesinternas.test;
+
+public class OuterClassesTest03 {
+
+    private String name = "William";
+
+    static class Nested {
+    private String lastName = "Suane";
+
+    void print() {
+
+        System.out.println(new OuterClassesTest03().name + " " + lastName);
+    }
+
+    }
+
+    public static void main(String[] args) {
+
+        OuterClassesTest03.Nested nested = new OuterClassesTest03.Nested();
+        nested.print();
+    }
+}
