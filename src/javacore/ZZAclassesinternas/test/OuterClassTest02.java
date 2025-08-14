f48b61dc07e594df245d3c28b1f686dcf2c2d9b7
RafaelMelo23 <eooraff@gmail.com>
2025-08-14 11:34:29 -0300

    aula190

---
+++ src/javacore/ZZAclassesinternas/test/OuterClassTest02.java
@@ -0,0 +1,25 @@
+package javacore.ZZAclassesinternas.test;
+
+public class OuterClassTest02 {
+
+    private String name = "Hego";
+
+    void print() {
+
+        String lastName = "Damask"; // Nesse caso, o java a considera efetivamente final, a nao ser que ela seja modificada dentro do codigo
+        class LocalClass {
+
+            void printLocalClass() {
+                System.out.println(name + " " + lastName); // <- a regra acima vale pro param tambem
+            }
+        }
+
+        new LocalClass().printLocalClass();
+    }
+
+    public static void main(String[] args) {
+
+        OuterClassTest02 outer = new OuterClassTest02();
+        outer.print();
+    }
+}
