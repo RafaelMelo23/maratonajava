eebbce1f83a7637ee383d712915abe5f1bb3525f
RafaelMelo23 <eooraff@gmail.com>
2025-08-15 15:06:21 -0300

    aula195

--- src/javacore/ZZBcomportamento/interfaces/CarPredicate.java
+++ src/javacore/ZZBcomportamento/interfaces/CarPredicate.java
@@ -2,7 +2,14 @@ package javacore.ZZBcomportamento.interfaces;
 
 import javacore.ZZBcomportamento.dominio.Car;
 
+@FunctionalInterface
 public interface CarPredicate {
 
+    // anonimas, funcoes, concisa
     boolean test(Car car);
+
+    // apenas 1 metodo abstrato
+    // (parametro) -> <expressao>
+    // (Car car) -> car.getColor().equals("green);
+
 }
