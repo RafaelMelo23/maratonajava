081d980c71ef97334f49939c14a86c0833703b83
RafaelMelo23 <eooraff@gmail.com>
2025-08-18 12:00:04 -0300

    aula204

--- src/javacore/ZZEstreams/test/StreamTest01.java
+++ src/javacore/ZZEstreams/test/StreamTest01.java
@@ -5,7 +5,6 @@ import javacore.ZZEstreams.dominio.LightNovel;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class StreamTest01 {
 
