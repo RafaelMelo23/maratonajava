f3985c549d2f0664ab5c800afe51f270f2dd00df
RafaelMelo23 <eooraff@gmail.com>
2025-08-27 12:12:06 -0300

    aula226

---
+++ src/javacore/ZZFthreads/test/ThreadSafeTest01.java
@@ -0,0 +1,36 @@
+package javacore.ZZFthreads.test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class ThreadSafeNames {
+
+    private final List<String> names = new ArrayList<>();
+
+    public synchronized void add(String name) {
+
+        names.add(name);
+    }
+
+    public synchronized void removeFirst() {
+
+        if (!names.isEmpty()) {
+            System.out.println(Thread.currentThread().getName());
+            System.out.println(Thread.currentThread().getName() + " removeu: " + names.removeFirst());
+        }
+    }
+}
+public class ThreadSafeTest01 {
+
+    public static void main(String[] args) {
+
+        ThreadSafeNames threadSafeNames = new ThreadSafeNames();
+        threadSafeNames.add("William Suane");
+
+        Runnable r = threadSafeNames::removeFirst;
+
+        new Thread(r).start();
+        new Thread(r).start();
+    }
+}
