4ac323dc7a164a7c90e91f1fa3ed8cd1945fb9a9
RafaelMelo23 <eooraff@gmail.com>
2025-08-29 18:57:48 -0300

    aula233

---
+++ src/javacore/ZZGconcorrencia/test/CopyOnWriteTest01.java
@@ -0,0 +1,51 @@
+package javacore.ZZGconcorrencia.test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.TimeUnit;
+
+public class CopyOnWriteTest01 {
+
+    public static void main(String[] args) {
+
+        List<Integer> list = new CopyOnWriteArrayList<>();
+
+        for (int i = 0; i < 2000; i++) {
+            list.add(i);
+        }
+
+        // [1]
+        // [1, 2]
+        // [1, 2, 3]
+        // [1, 2]
+
+        Runnable runnableIterator = () -> {
+
+            Iterator<Integer> iterator = list.iterator(); // o iterator mantem uma copia pro valor original, no momento da criacao
+
+            try {
+                TimeUnit.SECONDS.sleep(2);
+
+                iterator.forEachRemaining(System.out::println);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        };
+
+        Runnable runnableRemover = () -> {
+
+            for (int i = 0; i < 500; i++) {
+
+                System.out.printf("%sRemoved %d%n", Thread.currentThread().getName(), i);
+                list.remove(i);
+            }
+        };
+
+        new Thread(runnableIterator).start();
+        new Thread(runnableIterator).start();
+        new Thread(runnableRemover).start();
+    }
+}
