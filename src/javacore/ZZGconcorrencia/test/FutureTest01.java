00c840b4f27fbc117a76e8f40de484b58af42de4
RafaelMelo23 <eooraff@gmail.com>
2025-08-31 13:46:46 -0300

    aula239

---
+++ src/javacore/ZZGconcorrencia/test/FutureTest01.java
@@ -0,0 +1,34 @@
+package javacore.ZZGconcorrencia.test;
+
+import java.util.concurrent.*;
+
+public class FutureTest01 {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException, TimeoutException {
+
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        Future<Double> dollarFuture = executorService.submit(() -> {
+
+            TimeUnit.SECONDS.sleep(2);
+            return 4.35d;
+        });
+
+        System.out.println(doingSomething());
+        Double dollarResponse = dollarFuture.get(3, TimeUnit.SECONDS);
+        System.out.println("Dollar: " + dollarResponse);
+        executorService.shutdown();
+
+    }
+
+    private static long doingSomething() {
+
+        System.out.println(Thread.currentThread().getName());
+        long sum = 0;
+        for (int i = 0; i < 1_000_000; i++) {
+
+            sum += i;
+        }
+
+        return sum;
+    }
+}
