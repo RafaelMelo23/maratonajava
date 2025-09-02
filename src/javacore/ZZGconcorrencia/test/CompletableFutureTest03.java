50c6a60230234a38baa9d10332cd8b1273e66e92
RafaelMelo23 <eooraff@gmail.com>
2025-09-02 15:08:19 -0300

    aula242

---
+++ src/javacore/ZZGconcorrencia/test/CompletableFutureTest03.java
@@ -0,0 +1,41 @@
+package javacore.ZZGconcorrencia.test;
+
+import javacore.ZZGconcorrencia.service.StoreService;
+import javacore.ZZGconcorrencia.service.StoreServiceDeprecated;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class CompletableFutureTest03 {
+
+    public static void main(String[] args) {
+
+        StoreServiceDeprecated storeService = new StoreServiceDeprecated();
+        searchPricesAsyncCompletableFuture(storeService);
+    }
+
+    private static void searchPricesAsyncCompletableFuture(StoreServiceDeprecated storeService) {
+        long start = System.currentTimeMillis();
+
+        ExecutorService executorService = Executors.newFixedThreadPool(10, r -> {
+          Thread thread = new Thread(r);
+          thread.setDaemon(true);
+          return thread;
+        });
+        List<String> stores = List.of("Store 1", "Store 2", "Store 3", "Store 4");
+
+        List<CompletableFuture<Double>> pricesFuture = stores.stream()
+                .map(s -> CompletableFuture.supplyAsync(() -> storeService.getPriceSync(s), executorService))
+                .toList();
+
+        List<Double> pricesJoined = pricesFuture.stream()
+                .map(CompletableFuture::join)
+                .toList();
+
+        System.out.println(pricesJoined);
+        long end = System.currentTimeMillis();
+        System.out.printf("Time taken: %d ms\n", end - start);
+    }
+}
