829bbfd0db0988f22e455866b62b8479546e880e
RafaelMelo23 <eooraff@gmail.com>
2025-09-02 14:33:48 -0300

    aula241

---
+++ src/javacore/ZZGconcorrencia/test/CompletableFutureTest02.java
@@ -0,0 +1,32 @@
+package javacore.ZZGconcorrencia.test;
+
+import javacore.ZZGconcorrencia.service.StoreService;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+public class CompletableFutureTest02 {
+
+    public static void main(String[] args) {
+
+        StoreService storeService = new StoreService();
+        searchPricesAsyncCompletableFuture(storeService);
+    }
+
+    private static void searchPricesAsyncCompletableFuture(StoreService storeService) {
+        long start = System.currentTimeMillis();
+        List<String> stores = List.of("Store 1", "Store 2", "Store 3", "Store 4");
+
+        List<CompletableFuture<Double>> completableFutures = stores.stream()
+                .map(storeService::getPriceAsyncCompletableFuture)
+                .toList();
+
+        List<Double> prices = completableFutures.stream()
+                .map(CompletableFuture::join)
+                .toList();
+
+        System.out.println(prices);
+        long end = System.currentTimeMillis();
+        System.out.printf("Time taken: %d ms\n", end - start);
+    }
+}
