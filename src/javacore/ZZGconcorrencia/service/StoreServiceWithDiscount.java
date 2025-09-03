69ce81f6de0e8ed9b65e52e60dbe2a763e10d613
RafaelMelo23 <eooraff@gmail.com>
2025-09-03 13:42:10 -0300

    aula243

---
+++ src/javacore/ZZGconcorrencia/service/StoreServiceWithDiscount.java
@@ -0,0 +1,45 @@
+package javacore.ZZGconcorrencia.service;
+
+import javacore.ZZGconcorrencia.dominio.Discount;
+import javacore.ZZGconcorrencia.dominio.Quote;
+
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
+public class StoreServiceWithDiscount {
+
+    public String getPriceSync(String storeName) {
+        double price = priceGenerator();
+        Discount.Code discountCode = Discount.Code.values()[ThreadLocalRandom.current().nextInt(Discount.Code.values().length)];
+
+        return String.format("%s:%.2f:%s", storeName, price, discountCode);
+    }
+
+    public String applyDiscount(Quote quote) {
+        delay();
+
+        double discountedValue = quote.getPrice() * (100D - quote.getCode().getPercentage()) / 100;
+        return String.format("'%s original price: '%.2f' Applying discount code: '%s'. Final price: '%.2f'",
+                quote.getStore(),
+                quote.getPrice(),
+                quote.getCode(),
+                discountedValue);
+    }
+
+    private double priceGenerator() {
+        System.out.printf("%s generating price %n", Thread.currentThread().getName());
+
+        delay();
+        return ThreadLocalRandom.current().nextDouble(1, 500) * 10;
+    }
+
+    private void delay() {
+
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
---
+++ src/javacore/ZZGconcorrencia/test/CompletableFutureTest04.java
@@ -0,0 +1,27 @@
+package javacore.ZZGconcorrencia.test;
+
+import javacore.ZZGconcorrencia.dominio.Quote;
+import javacore.ZZGconcorrencia.service.StoreServiceWithDiscount;
+
+import java.util.List;
+
+public class CompletableFutureTest04 {
+
+    public static void main(String[] args) {
+
+        StoreServiceWithDiscount service = new StoreServiceWithDiscount();
+        searchPricesWithDiscount(service);
+    }
+
+    private static void searchPricesWithDiscount(StoreServiceWithDiscount service) {
+        List<String> stores = List.of("Store 1", "Store 2", "Store 3", "Store 4");
+
+//        stores.forEach(s -> System.out.println(service.getPriceSync(s)));
+
+        stores.stream()
+                .map(service::getPriceSync)
+                .map(Quote::newQuote)
+                .map(service::applyDiscount)
+                .forEach(System.out::println);
+    }
+}
