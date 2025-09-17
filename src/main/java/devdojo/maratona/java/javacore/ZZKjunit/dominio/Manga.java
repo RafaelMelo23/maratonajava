49fa44c8a00700c5ad81ee4f9f749d7b8e0be7dc
RafaelMelo23 <eooraff@gmail.com>
2025-09-17 13:01:05 -0300

    aula283

---
+++ src/main/java/devdojo/maratona/java/javacore/ZZKjunit/dominio/Manga.java
@@ -0,0 +1,11 @@
+package devdojo.maratona.java.javacore.ZZKjunit.dominio;
+
+import java.util.Objects;
+
+public record Manga(String name,
+                    int episodes) {
+
+    public Manga {
+        Objects.requireNonNull(name);
+    }
+}
